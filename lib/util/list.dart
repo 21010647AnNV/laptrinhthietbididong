214122466a9ca5e165dcf3f8e3f4b1bb3c25bd5c
nguyenan362 <21010647@st.phenikaa-uni.edu.vn>
2023-03-25 01:58:56 +0700

    first update

---
+++ lib/util/list.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
+import 'package:intl/intl.dart';
+
+class ListToDo {
+  String task;
+  String date;
+  
+
+  ListToDo({
+    required this.task,
+    required this.date,
+  });
+
+  
+
+}
